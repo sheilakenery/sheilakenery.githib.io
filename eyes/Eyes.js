173f951ecd495c132c9f9dc2a04373c9b659250b
sheilakenery <97704225+sheilakenery@users.noreply.github.com>
2022-04-03 22:27:11 -0700

    Update Eyes.js

--- eyes/Eyes.js
+++ eyes/Eyes.js
@@ -1,13 +1,12 @@
-//recall the div element from html file
-var balls = document.getElementsByClassName("ball");
+const balls = document.getElementsByClassName('ball');
 
-document.onmousemove = () => {
-  var x = (event.clientX * 100) / window.innerWidth + "%";
-  var y = (event.clientY * 100) / window.innerHeight + "%";
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
 
-  for (let i = 0; i < 2; i++) {
+  for(let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
-    balls[i].transfoorm = "translate(-" + x + ",-" + y + ")";
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
   }
 };
